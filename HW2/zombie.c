5ea9c1d54165df80dbc318d9a41db7c79871b9a5
Joey Cheung <30446022+JoeyCheung@users.noreply.github.com>
2019-09-26 22:31:57 -0400

    Add files via upload

---
+++ HW2/zombie.c
@@ -0,0 +1,25 @@
+// Alex Mancheno
+// CS 340 - Operating Systems (Spring 2018)
+// Zombie child process example
+
+#include <sys/wait.h>
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	int pid = fork();
+	if (pid == 0)
+	{
+		// Currently in child process
+		printf("I am the child with PID [%d] and my parent has PPID [%d].\n", getpid(), getppid());
+		sleep(1);
+		exit(0); // Child process terminates and enters zombie state
+	}
+	else
+	{
+		// Currently in parent process
+		printf("I am the parent and my id is [%d]\n", getpid());
+		sleep(30); // This sleep is what causes the child to be in zombie state
+	}
+	wait(NULL); // Terminate child
+}
